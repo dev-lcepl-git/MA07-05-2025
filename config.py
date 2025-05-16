34c515d5ef4ca51f16284f76e50a0163b809b7b3
pjpatil <pp892810@gmail.com>
2025-05-16 16:30:55 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,46 @@
+
+import mysql.connector
+import os
+
+# Get MySQL credentials from environment variables
+MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "admin")
+MYSQL_DB = os.getenv("MYSQL_DB", "test")
+
+# Connect to MySQL
+def get_db_connection():
+    return mysql.connector.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DB
+    )
+
+
+# import mysql.connector
+# from mysql.connector import Error, cursor
+#
+# # Database connection configuration
+# db_config = {
+#     'host': 'localhost',
+#     'user': 'root',
+#     'password': 'user123',
+#     'database': 'test'
+# }
+#
+#
+# # Sql connection function
+# def get_db_connection():
+#     try:
+#         connection = mysql.connector.connect(**db_config)
+#         if connection.is_connected():
+#             return connection
+#     except Error as e:
+#         print(f"Error connecting to database: {e}")
+#         return None
+#     # finally:
+#     #     if cursor:
+#     #         cursor.close()
+#     #     if connection:
+#     #         connection.close()
